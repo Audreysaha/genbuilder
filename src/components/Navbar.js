319b96a7cd5d4f8fa2784982d44f8367ca235210
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-07-04 05:47:04 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -481,9 +481,6 @@ const Navbar = ({
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
                 className="px-4 py-1.5 rounded-md text-sm bg-purple-500 hover:bg-purple-600 text-white dark:bg-purple-600 dark:hover:bg-purple-700"
-                onClick={() =>
-                  handleDeploy(html, cssWeb, "test1", activePageId)
-                }
                 href={projectPages.filter((page) => page.id == activePageId)[0]?.url}
               >
                 Open site
